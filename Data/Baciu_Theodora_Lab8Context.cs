3ab2b71181b44a983c7d822a17c438f352a67ef9
MALINA THEODORA BACIU <baciu.malina165912@student.ubbcluj.ro>
2020-11-29 15:50:44 +0200

    Add project files.

---
+++ Data/Baciu_Theodora_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Baciu_Theodora_Lab8.Models;
+
+namespace Baciu_Theodora_Lab8.Data
+{
+    public class Baciu_Theodora_Lab8Context : DbContext
+    {
+        public Baciu_Theodora_Lab8Context (DbContextOptions<Baciu_Theodora_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Baciu_Theodora_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Baciu_Theodora_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Baciu_Theodora_Lab8.Data;
+using Baciu_Theodora_Lab8.Models;
+
+namespace Baciu_Theodora_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Baciu_Theodora_Lab8.Data.Baciu_Theodora_Lab8Context _context;
+
+        public IndexModel(Baciu_Theodora_Lab8.Data.Baciu_Theodora_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+ .          Include(b => b.Publisher)
+ .          ToListAsync();
+        }
+    }
+}
